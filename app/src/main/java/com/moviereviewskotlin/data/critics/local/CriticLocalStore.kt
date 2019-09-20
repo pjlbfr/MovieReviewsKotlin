53af9f09335b49630a435b572b6f36168c205c83
Ashot <ashot@letmecode.org>
2019-09-20 11:11:52 +0300

    added CriticLocalStore

---
+++ app/src/main/java/com/moviereviewskotlin/data/critics/local/CriticLocalStore.kt
@@ -0,0 +1,27 @@
+package com.moviereviewskotlin.data.critics.local
+
+import com.moviereviewskotlin.data.critics.response.Critic
+import com.moviereviewskotlin.data.critics.response.Critics
+import com.moviereviewskotlin.room.dao.CriticDao
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class CriticLocalStore @Inject constructor(private val criticDao: CriticDao) {
+
+    fun getCritics(): Observable<MutableList<Critic>> =
+        Observable.create { e ->
+            val result = criticDao.getAll()
+            e.onNext(result)
+            e.onComplete()
+        }
+
+    fun updateCritics(critics: Critics): Observable<Critics> =
+        Observable.create { e ->
+            criticDao.deleteAll()
+            criticDao.insert(critics.results)
+            e.onNext(critics)
+            e.onComplete()
+        }
+
+
+}
